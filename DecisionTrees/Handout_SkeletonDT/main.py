652b3de9d4a8aa761527638edecc028e5fbdadb0
simon <simon.tenggren@gmail.com>
2019-11-17 02:56:23 +0100

    done, finally

--- DecisionTrees/Handout_SkeletonDT/main.py
+++ DecisionTrees/Handout_SkeletonDT/main.py
@@ -6,29 +6,50 @@ from sklearn import tree, metrics, datasets
 
 
 def main():
-    attributes, classes, data, target, data2, target2 = td.ToyData().get_data()
+    #attributes, classes, data, target, data2, target2 = td.ToyData().get_data()
     digits = datasets.load_digits()
     split = int(0.7 * len(digits.data))
     trainingFeatures = digits.data[:split]
-
     testFeatures = digits.data[split:]
     trainingLabels = digits.target[:split]
     testLabels = digits.target[split:]
     id3 = ID3.ID3DecisionTreeClassifier()
     classes = list(range(0, 10))
+
+    modifiedData = []
+    for row in trainingFeatures:
+        nArr = []
+        for pixel in row:
+            if pixel < 5:
+                nArr.append("d")
+            if 5 <= pixel < 10:
+                nArr.append("g")
+            if pixel >= 10:
+                nArr.append("l")
+        modifiedData.append(nArr)
     attributes = {}
-    #for index in range(64):
-    #    attributes[index] = list(range(0, 17))
+    for i in range(0, 64):
+        attributes["pixel" + str(i)] = ["d", "g", "l"]
 
-    attributes["dark"] = list(range(0, 5))
-    attributes["grey"] = list(range(5, 10))
-    attributes["light"] = list((range(10, 17)))
+    modifiedTestData = []
+    for row in testFeatures:
+        nArr = []
+        for pixel in row:
+            if pixel < 5:
+                nArr.append("d")
+            if 5 <= pixel < 10:
+                nArr.append("g")
+            if pixel >= 10:
+                nArr.append("l")
+        modifiedTestData.append(nArr)
 
-    myTree = id3.fit(trainingFeatures, trainingLabels, attributes, classes)
+    myTree = id3.fit(modifiedData, trainingLabels, attributes, classes)
     plot = id3.makeDotData()
     plot.render("testTree")
-    predicted = id3.predict(testFeatures, myTree)
+    predicted = id3.predict(modifiedTestData, myTree)
     print(predicted)
     print(metrics.classification_report(testLabels, predicted))
     print(metrics.confusion_matrix(testLabels, predicted))
+
+
 if __name__ == "__main__": main()
