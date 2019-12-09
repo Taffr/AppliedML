021a4990b758cdeded7d4fa936e39d4e5f893ce4
Jacob Rosensköld <mas15jro@student.lu.se>
2019-12-09 21:21:06 +0100

    refactorization

--- lab5/nearest_centroid_classifier.py
+++ lab5/nearest_centroid_classifier.py
@@ -1,27 +1,27 @@
 import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix
 
+
 class NearestCentroidClassifier:
     def fit(self, train_features, train_labels):
-        counts = {}
+        classification_count = {}
         mean_values = {}
 
         for i in range(len(train_features)):
-            pixel_values = train_features[i]
+            feature = train_features[i]
             label = train_labels[i]
 
-            if label in counts:
-                counts[label] += 1
-
-                for k in range(len(pixel_values)):
-                    mean_values[label][k] += pixel_values[k]
+            if label in classification_count:
+                classification_count[label] += 1
+                for k in range(len(feature)):
+                    mean_values[label][k] += feature[k]
             else:
-                counts[label] = 1
-                mean_values[label] = np.array(pixel_values)
+                classification_count[label] = 1
+                mean_values[label] = np.array(feature)
 
-        for digit in mean_values:
-            for i in range(len(mean_values[digit])):
-                mean_values[digit][i] /= counts[digit]
+        for mean_key, mean_value in mean_values.items():
+            for i in range(len(mean_value)):
+                mean_values[mean_key][i] /= classification_count[mean_key]
 
         self.mean_values = mean_values
 
@@ -31,10 +31,10 @@ class NearestCentroidClassifier:
         mean_values_keys = list(self.mean_values)
 
         for i in range(len(test_features)):
-            digit = np.array(test_features[i])
+            feature = np.array(test_features[i])
             distances = []
-            for l in self.mean_values:
-                d = np.linalg.norm(digit - self.mean_values[l])
+            for mean_value in self.mean_values.values():
+                d = np.linalg.norm(feature - mean_value)
                 distances.append(d)
             y_pred[i] = mean_values_keys[np.argmin(distances)]
 
